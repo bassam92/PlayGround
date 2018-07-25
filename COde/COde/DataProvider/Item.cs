43a0694738ce68bcba1a807495f563460eace6be
Mohamed Bassam <mbassam@csc.com>
2018-07-25 19:19:40 +0530

    Item Class added

---
+++ COde/COde/DataProvider/Item.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace COde.DataProvider
+{
+    internal class Item
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public DateTime ManufactoredDate { get; set; }
+        public bool Original { get; set; }
+    }
+}
---
+++ COde/COde/DataProvider/Reader.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace COde.DataProvider
+{
+    public class Reader
+    {
+        public void LoadJson()
+        {
+            using (StreamReader r = new StreamReader("file.json"))
+            {
+                string json = r.ReadToEnd();
+                List<Item> items = JsonConvert.DeserializeObject<List<Item>>(json);
+            }
+        }
+    }
+}
